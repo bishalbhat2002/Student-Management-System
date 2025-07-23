9ab06b51de77d1c5660a98f1c1aab47284a8f47e
Bishal Bhat <bishalbhat2002@gmail.com>
2025-07-23 09:31:29 +0545

    Resolve Merge Conflict

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 require_once "../../includes/show_message.php";
 ?>
 </body>
@@ -23,37 +22,4 @@ require_once "../../includes/show_message.php";
        }
 </script>
 
-=======
-       if(isset($_GET['error'])){
-              echo "<div id='error' class='position-bottom'>Error: {$_GET['error']}</div>";
-       }
-?>
-<?php
-       if(isset($_GET['success'])){
-              echo "<div id='success' class='position-bottom'>Success: {$_GET['success']}</div>";
-       }
-?>
-</body>
-<script src="../../js/script.js"></script>
-<script>
-       // JS code to Show Errors
-       window.onload = function hide(){
-       let errorElement =  document.getElementById('error');
-              if(errorElement){
-                     setTimeout(()=>{
-                            errorElement.classList.add('hide');
-                     }, 3000);
-              }       
-       
-       let successElement =  document.getElementById('success');
-              
-              if(successElement){
-                     setTimeout(()=>{
-                            successElement.classList.add('hide');
-                     }, 3000);
-              }
-       }
-
-</script>
->>>>>>> b3f5f87acc16104466d1bde0405f7c746e780acb
 </html>
